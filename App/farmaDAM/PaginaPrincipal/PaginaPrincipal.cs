5296adeb1f1c289ee342babce6d6d3c1330f6ba2
Gerard Lopez <gerard.lopezsds@gmail.com>
2017-11-30 16:58:48 +0100

    windows created

---
+++ App/farmaDAM/PaginaPrincipal/PaginaPrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaginaPrincipal
+{
+    public partial class PaginaPrincipal : Form
+    {
+        public PaginaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Form frm = new farmaDAM.AcercaDe();
+            frm.Show();
+        }
+    }
+}
