0914e3dd2177f5de160f135a4f24cbb2a66c2f22
Gerard Lopez <gerard.lopezsds@gmail.com>
2017-12-01 17:26:06 +0100

    Connection

---
+++ App/farmaDAM/ConexioBBDD/Conexio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace ConexioBBDD
+{
+    public class Conexio
+    {
+        MySql.Data.MySqlClient.MySqlConnection connexio;
+        string IPServidor =  "51.255.58.1";
+        string stringconnexio = "server=https://g2s2am.sdslab.cat:2083;uid=g2s2am;" + "pwd=12345aA;database=g2s2am_FarmaDAM;";
+        MySqlConnection databaseConnection = new MySqlConnection();
+        databaseConnection.
+
+
+    } 
+}
