553f27bac9dabdd23302e353b43632ed93f07131
Gerard Lopez <gerard.lopezsds@gmail.com>
2017-12-12 17:30:33 +0100

    Connection to bbdd

--- App/farmaDAM/PaginaPrincipal/PaginaPrincipal.cs
+++ App/farmaDAM/PaginaPrincipal/PaginaPrincipal.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ConexioBBDD;
 
 namespace PaginaPrincipal
 {
     public partial class PaginaPrincipal : Form
     {
+        ConexioBBDD.Conexio info = new ConexioBBDD.Conexio();
+
         public PaginaPrincipal()
         {
             InitializeComponent();
@@ -22,6 +25,13 @@ namespace PaginaPrincipal
 
             Form frm = new farmaDAM.AcercaDe();
             frm.Show();
+        }
+
+        private void buttonCreateTest_Click(object sender, EventArgs e)
+        {
+            string create = "CREATE TABLE TEST(test int(3))";
+            info.connexioBD(create);
+            
         }
     }
 }
