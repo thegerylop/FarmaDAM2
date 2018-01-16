e66d446999c18b0df82d49447260125da524305d
Unknown <gerard.lopezsds@gmail.com>
2018-01-16 18:04:58 +0100

    GridView

--- App/farmaDAM/LabFarm/Laboratoris.Designer.cs
+++ App/farmaDAM/LabFarm/Laboratoris.Designer.cs
@@ -33,79 +33,85 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnAceptar = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
+            this.BtnAct = new System.Windows.Forms.Button();
             this.CustomCIF = new CustomControlTB.CustomControlTB();
             this.CustomSocial = new CustomControlTB.CustomControlTB();
             this.CustomDen = new CustomControlTB.CustomControlTB();
             this.CustomCodi = new CustomControlTB.CustomControlTB();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inserirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnInserir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label3.Location = new System.Drawing.Point(166, 128);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.Size = new System.Drawing.Size(47, 18);
             this.label3.TabIndex = 14;
             this.label3.Text = "Codi :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label4.Location = new System.Drawing.Point(105, 180);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.Size = new System.Drawing.Size(104, 18);
             this.label4.TabIndex = 15;
             this.label4.Text = "Denominació :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label5.Location = new System.Drawing.Point(459, 128);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.Size = new System.Drawing.Size(87, 18);
             this.label5.TabIndex = 16;
             this.label5.Text = "Raó social :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label6.Location = new System.Drawing.Point(506, 180);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 20);
+            this.label6.Size = new System.Drawing.Size(39, 18);
             this.label6.TabIndex = 17;
             this.label6.Text = "CIF :";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(372, 48);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(203, 21);
+            this.label7.Size = new System.Drawing.Size(184, 20);
             this.label7.TabIndex = 18;
             this.label7.Text = "Laboratoris farmacèutics";
             // 
-            // button1
+            // BtnAceptar
             // 
-            this.button1.BackColor = System.Drawing.Color.DimGray;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(685, 233);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 26);
-            this.button1.TabIndex = 27;
-            this.button1.Text = "Aceptar";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BtnAceptar.BackColor = System.Drawing.Color.DimGray;
+            this.BtnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.BtnAceptar.ForeColor = System.Drawing.Color.White;
+            this.BtnAceptar.Location = new System.Drawing.Point(685, 233);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(77, 26);
+            this.BtnAceptar.TabIndex = 27;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = false;
+            this.BtnAceptar.Click += new System.EventHandler(this.SubmitLab);
             // 
             // dataGridView1
             // 
@@ -118,25 +124,25 @@
             this.dataGridView1.TabIndex = 28;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // button2
+            // BtnAct
             // 
-            this.button2.BackColor = System.Drawing.Color.DimGray;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(619, 397);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 26);
-            this.button2.TabIndex = 29;
-            this.button2.Text = "Actualitzar";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.BtnAct.BackColor = System.Drawing.Color.DimGray;
+            this.BtnAct.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnAct.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.BtnAct.ForeColor = System.Drawing.Color.White;
+            this.BtnAct.Location = new System.Drawing.Point(621, 352);
+            this.BtnAct.Name = "BtnAct";
+            this.BtnAct.Size = new System.Drawing.Size(91, 26);
+            this.BtnAct.TabIndex = 29;
+            this.BtnAct.Text = "Actualitzar";
+            this.BtnAct.UseVisualStyleBackColor = false;
+            this.BtnAct.Click += new System.EventHandler(this.button2_Click);
             // 
             // CustomCIF
             // 
             this.CustomCIF.Location = new System.Drawing.Point(565, 178);
             this.CustomCIF.Name = "CustomCIF";
-            this.CustomCIF.Options = CustomControlTB.CustomControlTB.Items.Password;
+            this.CustomCIF.Options = CustomControlTB.CustomControlTB.Items.Text;
             this.CustomCIF.Placeholder = null;
             this.CustomCIF.Size = new System.Drawing.Size(197, 27);
             this.CustomCIF.TabIndex = 13;
@@ -145,7 +151,7 @@
             // 
             this.CustomSocial.Location = new System.Drawing.Point(565, 126);
             this.CustomSocial.Name = "CustomSocial";
-            this.CustomSocial.Options = CustomControlTB.CustomControlTB.Items.Password;
+            this.CustomSocial.Options = CustomControlTB.CustomControlTB.Items.Text;
             this.CustomSocial.Placeholder = null;
             this.CustomSocial.Size = new System.Drawing.Size(197, 27);
             this.CustomSocial.TabIndex = 12;
@@ -154,7 +160,7 @@
             // 
             this.CustomDen.Location = new System.Drawing.Point(230, 178);
             this.CustomDen.Name = "CustomDen";
-            this.CustomDen.Options = CustomControlTB.CustomControlTB.Items.Password;
+            this.CustomDen.Options = CustomControlTB.CustomControlTB.Items.Text;
             this.CustomDen.Placeholder = null;
             this.CustomDen.Size = new System.Drawing.Size(197, 27);
             this.CustomDen.TabIndex = 11;
@@ -163,18 +169,72 @@
             // 
             this.CustomCodi.Location = new System.Drawing.Point(230, 126);
             this.CustomCodi.Name = "CustomCodi";
-            this.CustomCodi.Options = CustomControlTB.CustomControlTB.Items.Password;
+            this.CustomCodi.Options = CustomControlTB.CustomControlTB.Items.Text;
             this.CustomCodi.Placeholder = null;
             this.CustomCodi.Size = new System.Drawing.Size(197, 27);
             this.CustomCodi.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inserirToolStripMenuItem,
+            this.modificarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(913, 24);
+            this.menuStrip1.TabIndex = 30;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inserirToolStripMenuItem
+            // 
+            this.inserirToolStripMenuItem.Name = "inserirToolStripMenuItem";
+            this.inserirToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.inserirToolStripMenuItem.Text = "Inserir";
+            this.inserirToolStripMenuItem.Click += new System.EventHandler(this.inserirToolStripMenuItem_Click);
+            // 
+            // modificarToolStripMenuItem
+            // 
+            this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.modificarToolStripMenuItem.Text = "Modificar";
+            this.modificarToolStripMenuItem.Click += new System.EventHandler(this.modificarToolStripMenuItem_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.BackColor = System.Drawing.Color.DimGray;
+            this.BtnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.BtnEliminar.ForeColor = System.Drawing.Color.White;
+            this.BtnEliminar.Location = new System.Drawing.Point(621, 397);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(91, 26);
+            this.BtnEliminar.TabIndex = 31;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // BtnInserir
+            // 
+            this.BtnInserir.BackColor = System.Drawing.Color.DimGray;
+            this.BtnInserir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.BtnInserir.ForeColor = System.Drawing.Color.White;
+            this.BtnInserir.Location = new System.Drawing.Point(685, 233);
+            this.BtnInserir.Name = "BtnInserir";
+            this.BtnInserir.Size = new System.Drawing.Size(77, 26);
+            this.BtnInserir.TabIndex = 32;
+            this.BtnInserir.Text = "Inserir";
+            this.BtnInserir.UseVisualStyleBackColor = false;
+            this.BtnInserir.Visible = false;
             // 
             // Laboratoris
             // 
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(913, 476);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BtnInserir);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnAct);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnAceptar);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -184,10 +244,13 @@
             this.Controls.Add(this.CustomSocial);
             this.Controls.Add(this.CustomDen);
             this.Controls.Add(this.CustomCodi);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Laboratoris";
             this.Load += new System.EventHandler(this.Laboratoris_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,8 +267,13 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnAceptar;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BtnAct;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inserirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnInserir;
     }
 }
--- App/farmaDAM/LabFarm/Laboratoris.cs
+++ App/farmaDAM/LabFarm/Laboratoris.cs
@@ -11,21 +11,19 @@ namespace LabFarm
 {
     public partial class Laboratoris : Form
     {
+        string config = "SERVER= 51.255.58.1;PORT=3306;DATABASE=g2s2am_FarmaDAM;UID=g2s2am;PASSWORD=12345aA;";
+        MySqlConnection connexio = new MySqlConnection();
         public Laboratoris()
         {
             InitializeComponent();
         }
-        
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
         private void Laboratoris_Load(object sender, EventArgs e)
         {
+            string select = " * ";
+            string table = " laboratoris_farmaceutics";
             string config = "SERVER= 51.255.58.1;PORT=3306;DATABASE=g2s2am_FarmaDAM;UID=g2s2am;PASSWORD=12345aA;";
-            string query = "SELECT * FROM laboratoris_farmaceutics";
+            string query = "SELECT"+ select +"FROM" + table;
 
-            MySqlConnection connexio = new MySqlConnection();
             connexio.ConnectionString = config;
             connexio.Open();
 
@@ -40,20 +38,7 @@ namespace LabFarm
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string config = "SERVER= 51.255.58.1;PORT=3306;DATABASE=g2s2am_FarmaDAM;UID=g2s2am;PASSWORD=12345aA;";
-            string query = "SELECT * FROM laboratoris_farmaceutics";
-
-            MySqlConnection connexio = new MySqlConnection();
-            connexio.ConnectionString = config;
-            connexio.Open();
-
-            MySqlCommand command = new MySqlCommand(query, connexio);
-            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
-
-            DataTable data = new DataTable();
-            adapter.Fill(data);
-            dataGridView1.DataSource = data;
-            connexio.Close();
+            Laboratoris_Load(sender, e);
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -68,6 +53,49 @@ namespace LabFarm
                 CustomSocial.textBox1.Text = row.Cells[2].Value.ToString();
                 CustomCIF.textBox1.Text = row.Cells[3].Value.ToString();
             }
+        }
+
+        private void SubmitLab(object sender, EventArgs e)
+        {
+            string Codi = CustomCodi.textBox1.Text;
+            string Den = CustomDen.textBox1.Text;
+            string Soc = CustomSocial.textBox1.Text;
+            string CIF = CustomCIF.textBox1.Text;
+
+            string table = "laboratoris_farmaceutics";
+            string query = "UPDATE " + table + " SET codi_lab = " + Den + " ,rao_social = " + Soc + " ,cif = " + CIF + " WHERE id_lab = " + Codi;
+            connexio.ConnectionString = config;
+            try
+            {
+                MySqlCommand command = new MySqlCommand(query, connexio);
+                MySqlDataReader MyReader2;
+                connexio.Open();
+                MyReader2 = command.ExecuteReader();
+                MessageBox.Show("Data Updated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            connexio.Close();
+        }
+
+        private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BtnInserir.Visible = false;
+            BtnAceptar.Visible = true;
+            dataGridView1.Visible = true;
+            BtnAct.Visible = true;
+            BtnEliminar.Visible = true;
+        }
+
+        private void inserirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BtnInserir.Visible = true;
+            BtnAceptar.Visible = false;
+            dataGridView1.Visible = false;
+            BtnAct.Visible = false;
+            BtnEliminar.Visible = false;
         }
     }
 }
